ba131c5a936573c8d2e7712a3257ea8040e0f4a9
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-10-15 22:00:23 +0200

    Update 1.0.4

--- bot.cpp
+++ bot.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <tgbot/tgbot.h>
 #include <cpr/cpr.h>
-#include <json.hpp>
+#include <nlohmann/json.hpp>
+#include <map>
 
 const std::string WEATHER_COMMAND = "/weather";
 const std::string NEWS_COMMAND = "/news";
-const std::string API_KEY = "YOUR_API_KEY";
+const std::string LOCATION_COMMAND = "/location";
+const std::string API_KEY = "YOUR_API_KEY"; // CHANGE THIS
 const std::string API_URL = "https://api.weather.com/v2/pws/observations/current?stationId=KCASANFR58&format=json&units=m&apiKey=" + API_KEY;
 const std::map<std::string, std::string> city_news = {
     {"rome", "https://www.republica.it"},
     {"berlin", "https://www.berliner-zeitung.de"},
     {"paris", "https://www.lefigaro.fr"},
-    {"madrid", "https://elpais.com"}
+    {"madrid", "https://elpais.com"},
+    {"newyork", "https://www.nytimes.com"},
+    {"london", "https://www.theguardian.com"}
 };
 
 std::string parseWeatherInformation(const std::string& response, bool useFahrenheit) {
@@ -26,34 +30,41 @@ std::string parseWeatherInformation(const std::string& response, bool useFahrenh
   return "Temperature: " + std::to_string(temperature) + (useFahrenheit ? "°F" : "°C") + ", Humidity: " + std::to_string(humidity) + "%";
 }
 
+void sendMessageWithRetry(TgBot::Bot& bot, TgBot::Message::Ptr message, const std::string& text) {
+  try {
+    bot.getApi().sendMessage(message->chat->id, text);
+  } catch (const TgBot::TgException& e) {
+    std::cerr << "Error sending message: " << e.what() << std::endl;
+  }
+}
+
 void sendWeatherInformation(TgBot::Bot& bot, TgBot::Message::Ptr message, const std::string& location) {
   auto response = cpr::Get(cpr::Url{API_URL});
 
   if (response.status_code != 200) {
-    bot.getApi().sendMessage(message->chat->id, "An error occurred while fetching weather information. Please try again later.");
+    sendMessageWithRetry(bot, message, "An error occurred while fetching weather information. Please try again later.");
     return;
   }
 
   try {
     auto weatherInfo = parseWeatherInformation(response.text);
 
-    bot.getApi().sendMessage(message->chat->id, "The weather in " + location + " is: " + weatherInfo);
+    sendMessageWithRetry(bot, message, "The weather in " + location + " is: " + weatherInfo);
   } catch (const std::exception& e) {
-    bot.getApi().sendMessage(message->chat->id, "An error occurred while parsing weather information. Please try again later.");
-    return;
+    sendMessageWithRetry(bot, message, "An error occurred while parsing weather information. Please try again later.");
   }
 }
 
 void sendNewsInformation(TgBot::Bot& bot, TgBot::Message::Ptr message, const std::string& city) {
   auto it = city_news.find(city);
   if (it != city_news.end()) {
-    bot.getApi().sendMessage(message->chat->id, "Here is the link to the latest news in " + city + ": " + it->second);
+    sendMessageWithRetry(bot, message, "Here is the link to the latest news in " + city + ": " + it->second);
   } else {
-    bot.getApi().sendMessage(message->chat->id, "Sorry, news for the specified city is not available.");
+    sendMessageWithRetry(bot, message, "Sorry, news for the specified city is not available.");
   }
 }
 
-void sendCurrentLocation(TgBot::Bot& bot, TgBot::Message::Ptr message) {
+void sendCurrentLocationRequest(TgBot::Bot& bot, TgBot::Message::Ptr message) {
   TgBot::ReplyKeyboardMarkup::Ptr keyboard(new TgBot::ReplyKeyboardMarkup);
   keyboard->oneTimeKeyboard = true;
   keyboard->resizeKeyboard = true;
@@ -61,7 +72,7 @@ void sendCurrentLocation(TgBot::Bot& bot, TgBot::Message::Ptr message) {
     { TgBot::KeyboardButton::Ptr(new TgBot::KeyboardButton("Share Location", true, true)) }
   };
 
-  bot.getApi().sendMessage(message->chat->id, "Please share your location:", false, 0, keyboard);
+  sendMessageWithRetry(bot, message, "Please share your location:");
 }
 
 void handleLocation(TgBot::Bot& bot, TgBot::Message::Ptr message) {
@@ -69,24 +80,19 @@ void handleLocation(TgBot::Bot& bot, TgBot::Message::Ptr message) {
     double latitude = message->location->latitude;
     double longitude = message->location->longitude;
 
-    // You can use latitude and longitude to fetch weather information for the user's location
-    // Example:
-    // sendWeatherInformation(bot, message, "Your Location", latitude, longitude);
-
-    // For now, let's just reply with the coordinates
     std::string coordinates = "Latitude: " + std::to_string(latitude) + ", Longitude: " + std::to_string(longitude);
-    bot.getApi().sendMessage(message->chat->id, coordinates);
+    sendMessageWithRetry(bot, message, coordinates);
   } else {
-    bot.getApi().sendMessage(message->chat->id, "Location not found. Please try again.");
+    sendMessageWithRetry(bot, message, "Location not found. Please try again.");
   }
 }
 
 int main() {
-  TgBot::Bot bot("YOUR_BOT_TOKEN");
+  TgBot::Bot bot("YOUR_BOT_TOKEN"); // CHANGE THIS
 
   bot.getEvents().onCommand(WEATHER_COMMAND, [&bot](TgBot::Message::Ptr message) {
     if (message->text.length() < WEATHER_COMMAND.length() + 2) {
-      bot.getApi().sendMessage(message->chat->id, "Invalid location");
+      sendMessageWithRetry(bot, message, "Invalid location");
       return;
     }
 
@@ -106,7 +112,7 @@ int main() {
 
   bot.getEvents().onCommand(NEWS_COMMAND, [&bot](TgBot::Message::Ptr message) {
     if (message->text.length() < NEWS_COMMAND.length() + 2) {
-      bot.getApi().sendMessage(message->chat->id, "Invalid city");
+      sendMessageWithRetry(bot, message, "Invalid city");
       return;
     }
 
@@ -115,8 +121,8 @@ int main() {
     sendNewsInformation(bot, message, city);
   });
 
-  bot.getEvents().onCommand("location", [&bot](TgBot::Message::Ptr message) {
-    sendCurrentLocation(bot, message);
+  bot.getEvents().onCommand(LOCATION_COMMAND, [&bot](TgBot::Message::Ptr message) {
+    sendCurrentLocationRequest(bot, message);
   });
 
   bot.getEvents().onLocation([&bot](TgBot::Message::Ptr message) {
@@ -129,7 +135,7 @@ int main() {
     while (true) {
       longPoll.start();
     }
-  } catch (TgBot::TgException& e) {
+  } catch (const TgBot::TgException& e) {
     std::cerr << "Error: " << e.what() << std::endl;
   }
 
